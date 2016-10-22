a37548e9596980df62f68e123835b55989f0b2c6
Tibor Barsi <tiborbarsi@outlook.com>
2016-10-22 22:03:16 +0200

    tests: adds tests

--- tests/tests.js
+++ tests/tests.js
@@ -7,31 +7,54 @@ var Router = require('../router');
 
 
 function setup() {
-  var router = Object.assign(new Events(), Router, {
+  location.hash = '';
+
+  return Object.assign(new Events(), Router, {
     routes: {
       '/': 'home',
-      '/users/([0-9]+)': 'users:detail'
+      '/posts': 'posts:list',
+      '/posts/([0-9]+)/([a-z0-9-]+)': 'posts:detail'
     },
     defaultPath: '/'
   });
-  return router;
 }
 
 
-test('Router Test', function(t) {
-  var router = setup();
-  t.plan(4);
 
-  t.equal(location.hash.replace('#', ''), '', 'no hash');
+test('Router Init without hash', function(t) {
+  var router = setup();
+  t.plan(5);
 
   router.init();
-  t.equal(router._routes.home.toString(), '/^\\/$/', '.compile compiles regexp strings');
-  t.equal(location.hash.replace('#', ''), '/', '.defaultPath if no hash');
+  t.equal(location.hash.replace('#', ''), '/', '.init sets .defaultPath if No hash');
+
+  t.equal(router._routes.home.toString(), '/^\\/$/', '.compile compiles the regexp strings');
+
+  router.once('posts:list', function() {
+    t.pass('router calls .emit');});
+
+  router.once('posts:detail', function(year, slug) {
+    t.equal(year, '2016', 'router passes the first arg');
+    t.equal(slug, 'state-of-the-art', 'router passes the second arg');});
+
 
   setTimeout(function() {
-    router.on('users:detail', function(id) {
-      t.equal(id, '10', 'router calls .emit with arguments');
-    });
-    location.hash = '/users/10';
-  }, 100);
+    location.hash = '/posts';}, 100);
+
+  setTimeout(function() {
+    location.hash = '/posts/2016/state-of-the-art';}, 200);
+});
+
+
+
+test('Router Init with hash', function(t) {
+  var router = setup();
+  location.hash = '/posts';
+  t.plan(2);
+
+  router.once('posts:list', function() {
+    t.pass('.init calls .onHashChange if hash');});
+
+  router.init();
+  t.equal(location.hash.replace('#', ''), '/posts', '.init does Not set .defaultPath if hash');
 });
